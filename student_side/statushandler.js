0b34887995ee72099962e949013c0c562e9c7a75
Shilong Dai <sdai@viperfish.net>
2021-10-23 17:28:17 -0400

    tweaks

--- student_side/statushandler.js
+++ student_side/statushandler.js
@@ -1,39 +1,55 @@
 function display_skills(skill_type, skills) {
-    let skill_list = document.getElementById(skill_type).getElementsByTagName("ul")
+    let skill_list = document.getElementById(skill_type).getElementsByTagName("div")
     for (const skill of skills) {
-        let list_item = document.createElement("li")
-        list_item.setAttribute("class", "list-group-item")
         let skill_link = document.createElement("a")
+        skill_link.setAttribute("class", "list-group-item")
         skill_link.setAttribute("href", "/skills/" + skill.id)
         skill_link.innerText = skill.name
-        list_item.appendChild(skill_link)
-        skill_list[0].appendChild(list_item)
+        skill_list[0].appendChild(skill_link)
     }
+}
+
+function display_skill_count(status_data) {
+    let learnt_count = document.getElementById("learnt-count")
+    learnt_count.innerText = status_data.learnt_skills.length.toString()
+    let practice_count = document.getElementById("practice-count")
+    practice_count.innerText = status_data.wip_skills.length.toString()
+    let forgot_count = document.getElementById("forgot-count")
+    forgot_count.innerText = status_data.forgotten_skills.length.toString()
 }
 
 function display_all_skills(status_data) {
     display_skills("skill-learnt", status_data.learnt_skills)
     display_skills("needs-practice", status_data.wip_skills)
     display_skills("skill-forgot", status_data.forgotten_skills)
+    display_skill_count(status_data)
 }
 
 function display_questions(questions) {
-    let question_list = document.getElementById("question-summary").getElementsByTagName("ul")
-    for (const question of questions) {
-        let question_li = document.createElement("li")
-        if (question.correctness === 1) {
-            question_li.setAttribute("class", "list-group-item list-group-item-success")
-        } else if (question.correctness === 0) {
-            question_li.setAttribute("class", "list-group-item list-group-item-danger")
-        } else {
-            question_li.setAttribute("class", "list-group-item list-group-item-warning")
+    let question_block = document.getElementById("question-summary")
+    console.log(question_block)
+    let i, j, temporary, chunk = 6;
+    for (i = 0, j = questions.length; i < j; i += chunk) {
+        temporary = questions.slice(i, i + chunk);
+        let sublist = document.createElement("div")
+        sublist.setAttribute("class", "list-group list-group-horizontal")
+        for (const question of temporary) {
+            let question_link = document.createElement("a")
+            if (question.correctness === 1) {
+                question_link.setAttribute("class", "list-group-item list-group-item-success")
+            } else if (question.correctness === 0) {
+                question_link.setAttribute("class", "list-group-item list-group-item-danger")
+            } else {
+                question_link.setAttribute("class", "list-group-item list-group-item-warning")
+            }
+
+            question_link.setAttribute("href", "/questions/" + question.id)
+            question_link.innerText = "Question " + question.number
+            sublist.appendChild(question_link)
         }
-        let question_link = document.createElement("a")
-        question_link.setAttribute("href", "/questions/" + question.id)
-        question_link.innerText = "Question " + question.number
-        question_li.appendChild(question_link)
-        question_list[0].appendChild(question_li)
+        question_block.appendChild(sublist)
     }
+
 }
 
 function init() {
